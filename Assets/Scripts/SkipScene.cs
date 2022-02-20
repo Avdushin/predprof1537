bea83234c0d47b46fc78a0812347078f14a3ee2b
Avdushin <avdushinbeaver1@gmail.com>
2022-02-20 22:35:36 +0300

    init repo

---
+++ Assets/Scripts/SkipScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScene : MonoBehaviour
+{
+    public void OnClickLoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
